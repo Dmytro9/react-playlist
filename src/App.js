39058c2981f7f4c915898ba25d9b908eede768e9
Dmytro9 <dpoliuliuk@gmail.com>
2018-02-23 22:33:56 +0200

    some ui and mock data stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class Aggregate extends Component {
+let mockData = {
+  user: {
+    name: "Dima",
+    playlist: [
+      {
+        name: "My favorites 1",
+        songs: [
+          { name: "one", duration: 123 },
+          { name: "two", duration: 123 },
+          { name: "three", duration: 123 }
+        ]
+      },
+      {
+        name: "My favorites 2",
+        songs: [
+          { name: "one", duration: 456 },
+          { name: "two", duration: 465 },
+          { name: "three", duration: 456 }
+        ]
+      },
+      {
+        name: "My favorites 3",
+        songs: [
+          { name: "one", duration: 789 },
+          { name: "two", duration: 789 },
+          { name: "three", duration: 789 }
+        ]
+      },
+      {
+        name: "My favorites 4",
+        songs: [
+          { name: "one", duration: 159 },
+          { name: "two", duration: 159 },
+          { name: "three", duration: 753 }
+        ]
+      }
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{ width: "40%", display: "inline-block" }}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return [...songs, ...eachPlaylist.songs];
+    }, []);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0);
+
+    return (
+      <div style={{ width: "40%", display: "inline-block" }}>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
       </div>
     );
   }
@@ -38,19 +95,40 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        serverData: mockData
+      });
+    }, 3000);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Title</h1>
-        </header>
-        <Aggregate />
-        <Aggregate />
-        <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+        {this.state.serverData.user ? (
+          <div>
+            <h1 className="App-title">
+              {this.state.serverData.user.name}'s Title
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user.playlist} />
+            <HoursCounter playlists={this.state.serverData.user.playlist} />
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     );
   }
